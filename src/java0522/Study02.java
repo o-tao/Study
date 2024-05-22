dd1c9501ad6924951be3317d4d09d3f9439552fd
tao <dhtkdals369@gmail.com>
2024-05-22 18:14:24 +0900

    문제2) 별찍기 (다양한 삼각형)

---
+++ src/java0522/Study02.java
@@ -0,0 +1,79 @@
+package java0522;
+
+public class Study02 {
+
+	public static void main(String[] args) {
+
+		/***********************************************
+		 * 문제2) 2차원 배열를 이용하여 별(★)를 출력하시오
+		 * 출력 예시)
+	     * ★
+	     * ★★
+	     * ★★★
+	     * ★★★★
+	     * ★★★★★
+		 ************************************************/
+
+		// 해결 코드 작성 시작
+
+	    String 별 = "*";
+	
+	    /*** 코드 시작 ***/
+	    
+	    // 배열에 별 넣기
+//	    String[][] 배열 = new String[5][5];
+//
+//	    for (int i = 0; i < 배열.length; i++) {
+//	    	
+//	    	for (int j = 0; j < 배열[i].length; j++) {
+//	    		
+//	    		String 빈공간 = "";
+//	    		if (i == 0 && j <= 0) {
+//	    			배열[i][j] = 별;
+//	    			
+//	    		} else if (i == 1 && j <= 1) {
+//	    			배열[i][j] = 별;
+//	    			
+//	    		} else if (i == 2 && j <= 2) {
+//	    			배열[i][j] = 별;
+//	    			
+//	    		} else if (i == 3 && j <= 3) {
+//	    			배열[i][j] = 별;
+//	    			
+//	    		} else if (i == 4 && j <= 4) {
+//	    			배열[i][j] = 별;
+//	    			
+//	    		} else {
+//	    			배열[i][j] = 빈공간;
+//	    			
+//	    		}
+//	    		
+//	    		
+//	    	}
+//	    	
+//	    }
+//	    
+//	    // 배열에 값 출력
+//	    for (int i = 0; i < 배열.length; i++) {
+//	    	
+//	    	for (int j = 0; j < 배열[i].length; j++) {
+//	    		System.out.print(배열[i][j]);
+//	    		}
+//	    	System.out.println();
+//	    	}
+	    
+	    for (int i = 0; i < 5; i++) {
+	    	
+	    	for (int j = 0; j <= i; j++) {
+	    		System.out.print(별);
+	    	}
+	    	System.out.println();
+	    }
+	
+	
+	
+	    /*** 코드 종료 ***/
+
+  }
+
+}
