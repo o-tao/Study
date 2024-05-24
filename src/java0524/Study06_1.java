2f9071f48ce7723f6a6fc44a79d86074e93971b7
tao <dhtkdals369@gmail.com>
2024-05-24 16:14:30 +0900

    new 키워드를 생성하지 않고 다른 class 메소드 실행하기

---
+++ src/java0524/Study06_1.java
@@ -0,0 +1,10 @@
+package java0524;
+
+public class Study06_1 {
+	
+	void 함수() {
+		
+		
+	}
+
+}
---
+++ src/java0524/Study06_2.java
@@ -0,0 +1,12 @@
+package java0524;
+
+
+public class Study06_2 {
+	
+	static int a = 10;
+	
+	static void 함수() {
+		
+	}
+
+}
