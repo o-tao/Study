ea7724d84458bd774545bc5ce3b226b341ff62aa
tao <dhtkdals369@gmail.com>
2024-05-24 11:11:51 +0900

    메소드) 오버로딩 기본

---
+++ src/java0524/Study03.java
@@ -0,0 +1,24 @@
+package java0524;
+
+public class Study03 {
+
+	public static void main(String[] args) { // 오버로딩 기본
+		a();
+		a(1);
+		a("");
+
+	}
+	
+	static void a() {
+		
+	}
+	
+	static void a(int a) {
+		
+	}
+	
+	static void a(String a) {
+		
+	}
+
+}
