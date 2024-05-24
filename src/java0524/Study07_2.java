3ba66022c199bffb12ef75f14a013fcf537093a0
tao <dhtkdals369@gmail.com>
2024-05-24 16:17:00 +0900

    상속) 추상, Override

---
+++ src/java0524/Study07_2.java
@@ -0,0 +1,23 @@
+package java0524;
+
+public class Study07_2 extends Study07_1 {
+	
+	void 호출() { // method
+		함수();
+	}
+	
+	void 함수() {
+		System.out.println("함수111");
+	}
+	
+
+	@Override
+	void 함수2() { // Override
+		
+	}
+	
+	void 함수2(int a) { // OverLoding
+		
+	}
+
+}
